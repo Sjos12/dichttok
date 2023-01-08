9e75385bbaca6003944c64df4a724453d018f4d1
Denzel <Denzelstellingwerf@outlook.com>
2023-01-08 20:04:31 +0100

    Build for prod

---
+++ public/build/assets/Container.18a5d235.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as r,g as s,r as t}from"./app.185df2b5.js";const n={},a={class:"max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"};function c(o,_){return r(),s("div",a,[t(o.$slots,"default")])}const p=e(n,[["render",c]]);export{p as C};
