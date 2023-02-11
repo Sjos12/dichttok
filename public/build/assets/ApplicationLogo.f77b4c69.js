9f139e18db8c31b82980663150760e6eb14c6b77
Denzel <denzelstellingwerf@outlook.com>
2023-02-11 19:24:48 +0100

    build files

---
+++ public/build/assets/ApplicationLogo.f77b4c69.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,g as t}from"./app.0007a6e5.js";const c={},e={src:"/assets/logo.svg",alt:"DichtNet.",class:""};function r(n,a){return s(),t("img",e)}const l=o(c,[["render",r]]);export{l as A};
