05132136e320bb2ea8195c0336ba81a477922da6
Denzel <denzelstellingwerf@outlook.com>
2023-02-20 19:33:40 +0100

    gedicht now correctly sticks

--- resources/js/Pages/Home/Home.vue
+++ resources/js/Pages/Home/Home.vue
@@ -33,6 +33,7 @@ const stopTyping = ref(false);
 let charIndex = 0;
 const typeStatus = ref(false);
 let doodleIndex = 0;
+
 function register() {
     let url = route("register");
     Inertia.get(url);
@@ -66,6 +67,7 @@ function typeText() {
     // Wait
     setTimeout(typeText, textSpeed.value);
 }
+
 function eraseText() {
     if (stopTyping.value) return;
     if (!typeStatus) typeStatus.value = true;
@@ -109,7 +111,7 @@ function getRandomSVGUrl() {
 
 <template>
 
-    <div class="bg-gray-900 h-screen">
+    <div class="bg-gray-900 ">
         <nav class="z-50 relative shadow-lg w-full bg-gray-800 py-3">
             <container class="flex justify-between">
                 <ApplicationLogoVue class="text-white fill-white h-10"/>
@@ -132,7 +134,8 @@ function getRandomSVGUrl() {
                 <small>DichtNet is de</small>
                 <h1 class="text-white font-semibold text-6xl leading-tight ">De grootste gratis online dichtbundel.</h1>
                 <p>
-                    Het platform om nieuwe gedichten te vinden, je eigen gedichten te uploaden en ze te delen met je vrienden.
+                    Het platform om nieuwe gedichten te vinden, je eigen gedichten te uploaden en ze te delen met je
+                    vrienden.
                 </p>
                 <div class="flex gap-5 items-center">
                     <PrimaryButton @click="register" class="btn btn-primary  rounded-full px-6 py-2">
@@ -168,16 +171,16 @@ function getRandomSVGUrl() {
                     <div class="gedicht-height">
                         Input illustration here.
                     </div>
-<!--                    <MockGedicht class="snap-none" :gedicht="props.gedichten[0]"></MockGedicht>-->
+                    <!--                    <MockGedicht class="snap-none" :gedicht="props.gedichten[0]"></MockGedicht>-->
                     <!-- Library -->
-                    <MockGedicht class="sticky top-0" :gedicht="props.gedichten[1]"></MockGedicht>
-<!--                    <MockGedicht :gedicht="props.gedichten[2]"></MockGedicht>-->
-<!--                    &lt;!&ndash; Analysis &ndash;&gt;-->
 
-<!--                    <MockGedicht :is-highlighted="true" :gedicht="analysis_gedicht"></MockGedicht>-->
-<!--                    &lt;!&ndash; Create your own (gedicht 3)  &ndash;&gt;-->
-<!--                    <MockGedicht :gedicht="mockGedicht"></MockGedicht>-->
-<!--                    <MockGedicht :show-tags="true" :gedicht="props.gedichten[4]"></MockGedicht>-->
+                    <!--                    <MockGedicht :gedicht="props.gedichten[2]"></MockGedicht>-->
+                    <!--                    &lt;!&ndash; Analysis &ndash;&gt;-->
+
+                    <!--                    <MockGedicht :is-highlighted="true" :gedicht="analysis_gedicht"></MockGedicht>-->
+                    <!--                    &lt;!&ndash; Create your own (gedicht 3)  &ndash;&gt;-->
+                    <!--                    <MockGedicht :gedicht="mockGedicht"></MockGedicht>-->
+                    <!--                    <MockGedicht :show-tags="true" :gedicht="props.gedichten[4]"></MockGedicht>-->
                     <!--                    <MockGedicht></MockGedicht>-->
                     <!--                    <MockGedicht></MockGedicht>-->
                 </div>
@@ -201,9 +204,11 @@ function getRandomSVGUrl() {
                 <h2 class="text-white font-medium text-5xl leading-tight ">Een verzameling van honderden gratis
                     gedichten.</h2>
 
-                                <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolore eligendi ipsum, laborum magnam sed sint! At autem cupiditate doloremque facere fugiat reiciendis! Ab dolore laboriosam maiores saepe suscipit voluptatibus?
-                                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolore eligendi ipsum, laborum
+                    magnam sed sint! At autem cupiditate doloremque facere fugiat reiciendis! Ab dolore laboriosam
+                    maiores saepe suscipit voluptatibus?
+                </p>
 
                 <div class="flex gap-10">
                     <div class="grid">
@@ -222,77 +227,89 @@ function getRandomSVGUrl() {
                 </div>
             </div>
         </container>
-        <container class="gedicht-height grid grid-cols-12 snap-start">
-            <div class="col-span-6 my-auto gap-y-5 grid items-center">
-                <small>Met DichtNet</small>
-                <h2 class="text-white font-medium text-5xl leading-tight ">Analyseer je gedichten..</h2>
-                <p >
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolore eligendi ipsum, laborum
-                    magnam sed sint! At autem cupiditate doloremque facere fugiat reiciendis! Ab dolore laboriosam
-                    maiores saepe suscipit voluptatibus?
-                </p>
+
+        <!-- Show gedicht element -->
+        <div class="grid relative h-full">
+            <div class="absolute w-full h-full">
+                <container class=" h-full mx-auto grid grid-cols-12 ">
+                    <MockGedicht class="sticky gedicht-height col-start-8 col-end-12 top-20"
+                                 :gedicht="props.gedichten[1]"></MockGedicht>
+                </container>
 
             </div>
-        </container>
-        <container class="gedicht-height grid grid-cols-12 snap-start" >
-            <div class="col-span-7 my-auto gap-y-5 grid items-center">
-                <small>Met DichtNet</small>
-                <h2 class="text-white font-medium text-5xl leading-tight ">Deel je eigen gedichten met de wereld.</h2>
 
-                <div class="flex">
-                    <div class="card shadow-xl text-gray-200">
-                        <div class="grid gap-y-5">
-                            <div class="grid gap-y-3">
-                                <InputLabelVue>Titel van je gedicht*</InputLabelVue>
-                                <input
-                                    v-model="mockGedicht.titel"
-                                    spellcheck="false"
-                                    class="form-control text-gray-200 text-xl font-medium"
-                                    type="text"
-                                />
-                            </div>
-                            <div class="grid gap-y-3">
-                                <InputLabelVue>Jouw gedicht*</InputLabelVue>
+            <container class="gedicht-height grid grid-cols-12 ">
+                <div class="col-span-6 my-auto gap-y-5 grid items-center">
+                    <small>Met DichtNet</small>
+                    <h2 class="text-white font-medium text-5xl leading-tight ">Analyseer je gedichten..</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolore eligendi ipsum,
+                        laborum
+                        magnam sed sint! At autem cupiditate doloremque facere fugiat reiciendis! Ab dolore laboriosam
+                        maiores saepe suscipit voluptatibus?
+                    </p>
 
-                                <textarea
-                                    @input="() => stopTyping = true"
-                                    v-model="mockGedicht.gedicht"
-                                    spellcheck="false"
+                </div>
+            </container>
+            <container class="gedicht-height grid grid-cols-12 ">
+                <div class="col-span-7 my-auto gap-y-5 grid items-center">
+                    <small>Met DichtNet</small>
+                    <h2 class="text-white font-medium text-5xl leading-tight ">Deel je eigen gedichten met de
+                        wereld.</h2>
 
-                                    class="form-control h-64"
-                                ></textarea>
-                                <span class="ml-auto text-gray-300 font-thin text-sm">
+                    <div class="flex">
+                        <div class="card shadow-xl text-gray-200">
+                            <div class="grid gap-y-5">
+                                <div class="grid gap-y-3">
+                                    <InputLabelVue>Titel van je gedicht*</InputLabelVue>
+                                    <input
+                                        v-model="mockGedicht.titel"
+                                        spellcheck="false"
+                                        class="form-control text-gray-200 text-xl font-medium"
+                                        type="text"
+                                    />
+                                </div>
+                                <div class="grid gap-y-3">
+                                    <InputLabelVue>Jouw gedicht*</InputLabelVue>
+
+                                    <textarea
+                                        @input="() => stopTyping = true"
+                                        v-model="mockGedicht.gedicht"
+                                        spellcheck="false"
+
+                                        class="form-control h-64"
+                                    ></textarea>
+                                    <span class="ml-auto text-gray-300 font-thin text-sm">
                 {{ mockGedicht.gedicht.length + " / 1000" }} characters
                 </span>
+                                </div>
                             </div>
+                            <!--                        <div class="stepped-form-buttons">-->
+                            <!--                            <PrimaryButton>Submit</PrimaryButton>-->
+                            <!--                        </div>-->
                         </div>
-                        <!--                        <div class="stepped-form-buttons">-->
-                        <!--                            <PrimaryButton>Submit</PrimaryButton>-->
-                        <!--                        </div>-->
-                    </div>
-                    <div class="flex grow  justify-center items-center">
-                        <img class="animate-bounce duration-75 ease-in-out h-20 w-20 mx-auto text-indigo-500"
-                             src="/assets/undraw_arrow.svg" alt="Arrow to the right">
+                        <div class="flex grow  justify-center items-center">
+                            <img class="animate-bounce duration-75 ease-in-out h-20 w-20 mx-auto text-indigo-500"
+                                 src="/assets/undraw_arrow.svg" alt="Arrow to the right">
+                        </div>
                     </div>
                 </div>
-            </div>
-        </container>
+            </container>
+            <container class="gedicht-height grid grid-cols-12 ">
+                <div class="col-span-6 my-auto gap-y-5 grid items-center">
+                    <small>DichtNet heeft</small>
+                    <h2 class="text-white font-medium text-5xl leading-tight ">Alle genre’s op een plek.</h2>
+                    <p>
+                        Iedereen heeft zijn eigen voorkeuren. Daarom kan je gedichten filtreren op basis van een genre.
+                        Hierdoor vind je makkelijker gedichten die je zelf interessant vindt.
+                    </p>
+                    <div class="flex gap-4 flex-wrap w-full justify-start">
+                        <div
+                            v-for="genre of props.genres"
+                            :key="genre.id"
+                            :style="{backgroundColor: genre.color}"
 
-        <container class="gedicht-height grid grid-cols-12 snap-start">
-            <div class="col-span-6 my-auto gap-y-5 grid items-center">
-                <small>DichtNet heeft</small>
-                <h2 class="text-white font-medium text-5xl leading-tight ">Alle genre’s op een plek.</h2>
-                <p >
-                    Iedereen heeft zijn eigen voorkeuren. Daarom kan je gedichten filtreren op basis van een genre.
-                    Hierdoor vind je makkelijker gedichten die je zelf interessant vindt.
-                </p>
-                <div class="flex gap-4 flex-wrap w-full justify-start">
-                    <div
-                        v-for="genre of props.genres"
-                        :key="genre.id"
-                        :style="{backgroundColor: genre.color}"
-
-                        class="
+                            class="
                                 rounded-full
                                 justify-center
                                 items-center
@@ -306,16 +323,20 @@ function getRandomSVGUrl() {
                                 text-xs text-white
                             "
 
-                    >
-                        {{ genre.name }}
+                        >
+                            {{ genre.name }}
+                        </div>
                     </div>
-                </div>
 
-            </div>
-        </container>
+                </div>
+            </container>
+        </div>
+
         <container class="gedicht-height snap-start grid grid-cols-12 snap-start">
-            <div class="col-start-2 col-end-11 my-auto px-10 py-20  bg-gray-800 shadow-xl rounded-md hover:drop-shadow-xl gap-y-5 grid items-center justify-center gap-5">
-                <img src="/assets/gedicht_lady.jpg" class="w-full border-2 border-indigo-500 h-72 rounded-md shadow- object-cover object-top" alt="">
+            <div
+                class="col-start-2 col-end-11 my-auto px-10 py-20  bg-gray-800 shadow-xl rounded-md hover:drop-shadow-xl gap-y-5 grid items-center justify-center gap-5">
+                <img src="/assets/gedicht_lady.jpg"
+                     class="w-full border-2 border-indigo-500 h-72 rounded-md shadow- object-cover object-top" alt="">
                 <h2 class="text-white font-medium text-4xl leading-tight text-center ">
                     Overtuigd?
                     Sluit je gratis aan!</h2>
@@ -327,7 +348,8 @@ function getRandomSVGUrl() {
         </container>
         <div class="relative bottom-0 w-full bg-gray-800 py-4 snap-start">
             <container class="mx-auto w-full">
-                <small class="mx-auto text-center font-thin text-sm text-gray-300 ">Copyright Denzel Stellingwerf 2023.</small>
+                <small class="mx-auto text-center font-thin text-sm text-gray-300 ">Copyright Denzel Stellingwerf
+                    2023.</small>
             </container>
         </div>
     </div>
